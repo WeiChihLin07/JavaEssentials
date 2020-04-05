871b008943f2981abbaaa976ed7b3566d03cbbd1
Weichihlih07 <weichihlin@my.jcu.edu.au>
2020-04-05 22:54:39 +1000

    Convert primitive values to strings

--- src/au/university/Main.java
+++ src/au/university/Main.java
@@ -6,5 +6,25 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        var result2 = 10 + 20;
+        System.out.println(result2);
+
+        // Convert primitive values to strings
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
+
+        // Underscore is just for readability
+        long longValue = 10_000_000;
+        var fromLong = Long.toString(longValue);
+        System.out.println(fromLong);
     }
 }
