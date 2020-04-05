2e2677b10c49a9af38fc9e44c316837c6149f07c
Weichihlih07 <weichihlin@my.jcu.edu.au>
2020-04-05 22:32:06 +1000

    Initial commit

---
+++ src/au/university/Main.java
@@ -0,0 +1,10 @@
+package au.university;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 10 + 12;
+        String answer = "The answer is " + result;
+        System.out.println(answer);
+    }
+}
